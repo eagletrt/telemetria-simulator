2b036ad0a8764fc361bc243647f7819ef884a9d9
euberdeveloper <euberdeveloper@gmail.com>
2020-11-07 00:58:47 +0100

    feat(bin): cli added

--- source/bin/index.ts
+++ source/bin/index.ts
@@ -2,6 +2,7 @@
 import * as yargs from 'yargs';
 import { virtualizeCan, VirtualizeCanOptions } from '../lib/modules/virtualizeCan';
 import { simulateCan, SimulateCanOptions } from '../lib/modules/simulateCan';
+import { simulateGps, SimulateGpsOptions } from '../lib/modules/simulateGps';
 
 yargs
     .scriptName('eagle')
@@ -43,13 +44,49 @@ yargs
     )
     .command(
         'simulate',
-        'Simulate a canbus or a gps',
+        'Simulate a canbus or a gps or both',
         yargs => {
             yargs
                 .command(
                     'can',
                     'Simulates a canbus by sending messages from a log',
-                    () => {},
+                    (yargs) => {
+                        yargs
+                            .options({
+                                'log': {
+                                    alias: 'l',
+                                    default: null,
+                                    describe: 'The log file that contains the messages to be sent over the can',
+                                    defaultDescription: 'There is a module built-in default can log if nothing is specified',
+                                    type: 'string'
+                                },
+                                'can-interface': {
+                                    alias: 'i',
+                                    default: 'can0',
+                                    describe: 'The name of the can interface to create',
+                                    type: 'string'
+                                },
+                                'silent': {
+                                    alias: 's',
+                                    default: false,
+                                    describe: 'If logs will be displayed',
+                                    type: 'boolean'
+                                },
+                                'simulate-time': {
+                                    alias: 't',
+                                    default: true,
+                                    describe: 'If the time of the can log will be simulated',
+                                    type: 'boolean'
+                                },
+                                'iterations': {
+                                    alias: 'n',
+                                    default: Infinity,
+                                    describe: 'The number of iterations in which the can log file will be sent to the canbus',
+                                    defaultDescription: 'If nothing is specified, the file is sent in an infinite loop',
+                                    type: 'number'
+                                }
+                            });
+                    },
                     async argv => {
                         const args: any = argv;
                         const log = args.log;
@@ -62,41 +99,106 @@ yargs
                         await simulateCan(log, options);
                     }
                 )
-                .demandCommand(1, 'You must specify either the command "can" or the command "gps"')
-                .options({
-                    'log': {
-                        alias: 'l',
-                        default: null,
-                        describe: 'The log file that contains the messages to be sent over the can',
-                        defaultDescription: 'There is a module built-in default canlog if nothing is specified',
-                        type: 'string'
+                .command(
+                    'gps',
+                    'Simulates a gps by sending messages from a log',
+                    (yargs) => {
+                        yargs
+                            .options({
+                                'log': {
+                                    alias: 'l',
+                                    default: null,
+                                    describe: 'The log file that contains the messages to be sent over the gps',
+                                    defaultDescription: 'There is a module built-in default gps log if nothing is specified',
+                                    type: 'string'
+                                },
+                                'silent': {
+                                    alias: 's',
+                                    default: false,
+                                    describe: 'If logs will be displayed',
+                                    type: 'boolean'
+                                }
+                            });
                     },
-                    'can-interface': {
-                        alias: 'i',
-                        default: 'can0',
-                        describe: 'The name of the can interface to create',
-                        type: 'string'
-                    },
-                    'silent': {
-                        alias: 's',
-                        default: false,
-                        describe: 'If logs will be displayed',
-                        type: 'boolean'
-                    },
-                    'simulate-time': {
-                        alias: 't',
-                        default: true,
-                        describe: 'If the time of the can log will be simulated',
-                        type: 'boolean'
-                    },
-                    'iterations': {
-                        alias: 'n',
-                        default: Infinity,
-                        describe: 'The number of iterations in which the can log file will be sent to the canbus',
-                        defaultDescription: 'If nothing is specified, the file is sent in an infinite loop',
-                        type: 'number'
+                    async argv => {
+                        const args: any = argv;
+                        const log = args.log;
+                        const options: SimulateGpsOptions = {
+                            silent: args.silent,
+                        };
+                        await simulateGps(log, options);
                     }
-                })
+                )
+                .command(
+                    'all',
+                    'Simulates both a canbus and a gps',
+                    (yargs) => {
+                        yargs
+                            .options({
+                                'can-log': {
+                                    alias: 'c',
+                                    default: null,
+                                    describe: 'The log file that contains the messages to be sent over the can',
+                                    defaultDescription: 'There is a module built-in default can log if nothing is specified',
+                                    type: 'string'
+                                },
+                                'gps-log': {
+                                    alias: 'g',
+                                    default: null,
+                                    describe: 'The log file that contains the messages to be sent over the gps',
+                                    defaultDescription: 'There is a module built-in default gps log if nothing is specified',
+                                    type: 'string'
+                                },
+                                'can-interface': {
+                                    alias: 'i',
+                                    default: 'can0',
+                                    describe: 'The name of the can interface to create',
+                                    type: 'string'
+                                },
+                                'silent': {
+                                    alias: 's',
+                                    default: false,
+                                    describe: 'If logs will be displayed',
+                                    type: 'boolean'
+                                },
+                                'simulate-time': {
+                                    alias: 't',
+                                    default: true,
+                                    describe: 'If the time of the can log will be simulated',
+                                    type: 'boolean'
+                                },
+                                'iterations': {
+                                    alias: 'n',
+                                    default: Infinity,
+                                    describe: 'The number of iterations in which the can log file will be sent to the canbus',
+                                    defaultDescription: 'If nothing is specified, the file is sent in an infinite loop',
+                                    type: 'number'
+                                }
+                            });
+                    },
+                    async argv => {
+                        const args: any = argv;
+
+                        const canLog = args.canLog;
+                        const gpsLog = args.gpsLog;
+            
+                        const canOptions: SimulateCanOptions = {
+                            canInterface: args.canInterface,
+                            silent: args.silent,
+                            iterations: args.iterations,
+                            simulateTime: args.simulateTime
+                        };
+                        const gpsOptions: SimulateGpsOptions = {
+                            silent: args.silent,
+                        };
+            
+                        await Promise.all([
+                            simulateCan(canLog, canOptions),
+                            simulateGps(gpsLog, gpsOptions)
+                        ]);
+                    }
+                )
+                .demandCommand(1, 'You must use "can", "gps" or "all" command')
                 .argv;
         }
     )
